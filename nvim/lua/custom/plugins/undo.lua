0b453200382e0c9ca449fc95147504b3bc6d329a
therealnuclearhippo <145872535+therealnuclearhippo@users.noreply.github.com>
2023-09-23 15:26:03 -0500

    Add files via upload
    Requires ripgrep for grep functionality and an installation of delta (https://github.com/dandavison/delta/releases/tag/0.16.5) for the diff to function correctly.

---
+++ nvim/lua/custom/plugins/undo.lua
@@ -0,0 +1,25 @@
+return {
+
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"debugloop/telescope-undo.nvim",
+	},
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				undo = {
+					-- telescope-undo.nvim config, see below
+					side_by_side = true,
+					layout_strategy = "vertical",
+					layout_config = {
+						preview_height = 0.8,
+					},
+				},
+			},
+		})
+		require("telescope").load_extension("undo")
+		vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+	end,
+}
+
